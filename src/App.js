79fdd2fb480425ff77d050f64a80428d11ad602d
Oscar Sánchez <ojsm45@gmail.com>
2019-10-24 23:58:18 -0300

    added prop-types

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './components/Header';
+import NuevaCita from './components/NuevaCita';
+import ListaCitas from './components/ListaCitas';
+
+class App extends Component {
+  state = {
+    citas: []
+  };
+
+  componentDidMount() {
+    const citasLS = localStorage.getItem('citas');
+
+    if (citasLS)
+      this.setState({
+        citas: JSON.parse(citasLS)
+      });
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem('citas', JSON.stringify(this.state.citas));
+  }
+
+  crearNuevaCita = datos => {
+    const citas = [...this.state.citas, datos];
+
+    this.setState({
+      citas
+    });
+  };
+
+  eliminarCita = id => {
+    const citasActuales = [...this.state.citas];
+
+    const citas = citasActuales.filter(item => item.id !== id);
+
+    this.setState({
+      citas
+    });
+  };
+
+  render() {
+    return (
+      <div className='container'>
+        <Header titulo='Administrador pacientes veterinaria' />
+        <div className='row'>
+          <div className='col-md-10 mx-auto'>
+            <NuevaCita crearNuevaCita={this.crearNuevaCita} />
+          </div>
+          <div className='mt-5 col-md-10 mx-auto'>
+            <ListaCitas
+              citas={this.state.citas}
+              eliminarCita={this.eliminarCita}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
